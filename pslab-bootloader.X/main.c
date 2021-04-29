05e8b26194ecfb0874bee10f91152a4529287271
Alexander Bessman <alexander@bessman.se>
2021-04-29 16:18:42 +0200

    Remove Microchip's autogenerated header from main.c

--- pslab-bootloader.X/main.c
+++ pslab-bootloader.X/main.c
@@ -1,50 +1,13 @@
 /**
-  Generated main.c file from MPLAB Code Configurator
-
-  @Company
-    Microchip Technology Inc.
-
-  @File Name
-    main.c
-
-  @Summary
-    This is the generated main.c using PIC24 / dsPIC33 / PIC32MM MCUs.
-
-  @Description
-    This source file provides main entry point for system initialization and application code development.
-    Generation Information :
-        Product Revision  :  PIC24 / dsPIC33 / PIC32MM MCUs - 1.170.0
-        Device            :  PIC24EP256GP204
-    The generated drivers are tested against the following:
-        Compiler          :  XC16 v1.61
-        MPLAB 	          :  MPLAB X v5.45
+ * @file main.c
+ * @brief PSLab bootloader.
+ * 
+ * The bootloader loads the main application, if one exists. If the GPIO pin
+ * (RC5) is grounded, the device will stay in bootloader mode even if an
+ * application exists. While in bootloader mode, a new application can be
+ * flashed with the Unified Bootloader Host Application, or a similar tool.
  */
 
-/*
-    (c) 2020 Microchip Technology Inc. and its subsidiaries. You may use this
-    software and any derivatives exclusively with Microchip products.
-
-    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
-    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
-    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
-    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
-    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
-
-    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
-    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
-    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
-    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
-    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
-    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
-    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
-
-    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
-    TERMS.
- */
-
-/**
-  Section: Included Files
- */
 #include "mcc_generated_files/system.h"
 #include "mcc_generated_files/boot/boot_process.h"
 #include "mcc_generated_files/clock.h"
@@ -53,9 +16,6 @@
 
 #include "rgb_led.h"
 
-/*
-                         Main application
- */
 int main(void)
 {
     // initialize the device
@@ -87,7 +47,3 @@ int main(void)
 
     return 1;
 }
-/**
- End of File
- */
-
