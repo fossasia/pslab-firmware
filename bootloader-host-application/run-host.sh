dfa4c9ee8808dd2d069295f365d60aaf9d42dac9
Padmal <CloudyPadmal@users.noreply.github.com>
2021-06-17 14:29:09 +0200

    feat: update host application run script
    added a check to branch through macos an ubuntu to run host application

--- bootloader-host-application/run-host.sh
+++ bootloader-host-application/run-host.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-java --module-path /usr/local/openjfx/lib --add-modules=javafx.controls,javafx.fxml,javafx.base,javafx.media,javafx.web,javafx.swing -jar 'UnifiedHost-1.17.0.jar'
-
+if [ "$(uname)" == "Darwin" ]; then
+    java --module-path /usr/local/openjfx/lib --add-modules=javafx.controls,javafx.fxml,javafx.base,javafx.media,javafx.web,javafx.swing -jar 'UnifiedHost-1.17.0.jar'    
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    [ ! -d "/usr/share/openjfx/lib" ] && sudo apt install openjfx
+    java --module-path /usr/share/openjfx/lib --add-modules=javafx.controls,javafx.fxml,javafx.base,javafx.media,javafx.web,javafx.swing -jar 'UnifiedHost-1.17.0.jar'
+fi
