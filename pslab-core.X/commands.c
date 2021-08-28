1008bd813e59a4699ca0de0ace6ce763928aecc0
Alexander Bessman <alexander@bessman.se>
2021-08-28 14:59:59 +0200

    Oscilliscope: Implement two, three, and four channel mode

--- pslab-core.X/commands.c
+++ pslab-core.X/commands.c
@@ -89,17 +89,17 @@ command_func_t* const cmd_table[NUM_PRIMARY_CMDS + 1][NUM_SECONDARY_CMDS_MAX + 1
     },
     { // 2 ADC
      // 0                          1 CAPTURE_ONE                2 CAPTURE_TWO                   3 CAPTURE_DMASPEED
-        Undefined,                 OSCILLOSCOPE_CaptureOne,     Unimplemented,                  Unimplemented,
+        Undefined,                 OSCILLOSCOPE_CaptureOne,     OSCILLOSCOPE_CaptureTwo,        Unimplemented,
      // 4 CAPTURE_FOUR             5 CONFIGURE_TRIGGER          6 GET_CAPTURE_STATUS            7 GET_CAPTURE_CHANNEL
-        Unimplemented,             Unimplemented,               OSCILLOSCOPE_GetCaptureStatus,  Unimplemented,
+        OSCILLOSCOPE_CaptureFour,  Unimplemented,               OSCILLOSCOPE_GetCaptureStatus,  Unimplemented,
      // 8 SET_PGA_GAIN             9 GET_VOLTAGE                10 GET_VOLTAGE_SUMMED           11 START_ADC_STREAMING
         Unimplemented,             MULTIMETER_GetVoltage,       MULTIMETER_GetVoltageSummed,    Removed,
      // 12 SELECT_PGA_CHANNEL      13 CAPTURE_12BIT             14 CAPTURE_MULTIPLE             15 SET_HI_CAPTURE
         Unimplemented,             Unimplemented,               Removed,                        Unimplemented,
      // 16 SET_LO_CAPTURE          17 SET_HI_CAPTURE12          18 SET_LO_CAPTURE12             19 CAPTURE_DMASPEED12
         Unimplemented,             Unimplemented,               Unimplemented,                  Unimplemented,
-     // 20 MULTIPOINT_CAPACITANCE  21 SET_CAP                   22 PULSE_TRAIN                  23
-        Unimplemented,             Unimplemented,               Unimplemented,                  Undefined,
+     // 20 MULTIPOINT_CAPACITANCE  21 SET_CAP                   22 PULSE_TRAIN                  23 CAPTURE_THREE
+        Unimplemented,             Unimplemented,               Unimplemented,                  OSCILLOSCOPE_CaptureThree,
      // 24                         25                           26                              27
         Undefined,                 Undefined,                   Undefined,                      Undefined,
     },
--- pslab-core.X/instruments/oscilloscope.h
+++ pslab-core.X/instruments/oscilloscope.h
@@ -6,13 +6,38 @@
  * 
  * @description
  * This command function takes three arguments over serial:
- * 1. (uint8) The pin to sample
- * 2. (uint16) The number of samples to capture
- * 3. (uint16) The time to wait between samples in instruction cycles
+ * 1. (uint8)  Configuration byte; The first four LSB encode channel one mapping,
+ *             the fifth LSB encodes channels two, three, and four mappings, and
+ *             the MSB enables the trigger.
+ * 2. (uint16) The number of samples to capture.
+ * 3. (uint16) The time to wait between samples in instruction cycles.
  * It returns nothing over serial.
  * It sends an acknowledge byte (SUCCESS).
+ * 
+ * @return SUCCESS
  */
 response_t OSCILLOSCOPE_CaptureOne(void);
+
+/**
+ * @brief Capture samples on two channels simultaneously.
+ */
+response_t OSCILLOSCOPE_CaptureTwo(void);
+
+/**
+ * @brief Capture samples on three channels simultaneously.
+ * 
+ * @description
+ * Since the MCU only supports 1, 2, or 4 simultaneous channels, this is
+ * accomplished by sampling four channels simultaneously and discarding the
+ * samples from the fourth. This function therefore has the same sample rate
+ * as OSCILLOSCOPE_CaptureFour, but greater sample depth.
+ */
+response_t OSCILLOSCOPE_CaptureThree(void);
+
+/**
+ * @brief Capture samples on four channels simultaneously.
+ */
+response_t OSCILLOSCOPE_CaptureFour(void);
 
 /**
  * @brief
