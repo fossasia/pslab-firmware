ff4c4a02c881ba86b77ff6dc0483cecbea82216e
Padmal <CloudyPadmal@users.noreply.github.com>
2021-10-17 09:03:03 +0200

    feat: device registers related functions

---
+++ pslab-core.X/helpers/device.h
@@ -0,0 +1,37 @@
+#ifndef VERSION_H
+#define	VERSION_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    /**
+     * @brief Get hardware version.
+     * @return DO_NOT_BOTHER
+     */
+    response_t DEVICE_GetVersion(void);
+    
+    /**
+     * @brief Reset the device by moving program counter to 0x0000
+     * @return DO_NOT_BOTHER
+     */
+    response_t DEVICE_Reset(void);
+    
+    /**
+     * @brief Read content from Special Function Registers
+     * @return SUCCESS
+     */
+    response_t DEVICE_ReadRegisterData(void);
+    
+    /**
+     * @brief Write content to Special Function Registers given the register 
+     * address and data as two integers
+     * @return SUCCESS
+     */
+    response_t DEVICE_WriteRegisterData(void);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* VERSION_H */
