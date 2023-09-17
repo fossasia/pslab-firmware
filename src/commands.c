2eb14f9063cd30c3124c7028d6261588da97d37b
Alexander Bessman <alexander@bessman.se>
2023-09-17 20:13:48 +0200

    Add command to read firmware version

--- src/commands.c
+++ src/commands.c
@@ -248,7 +248,7 @@ command_func_t* const cmd_table[NUM_PRIMARY_CMDS + 1][NUM_SECONDARY_CMDS_MAX + 1
      // 0                               1 GET_CTMU_VOLTAGE              2 GET_CAPACITANCE               3 GET_FREQUENCY
         Undefined,                      MULTIMETER_GetCTMUVolts,        MULTIMETER_GetCapacitance,      Unimplemented,
      // 4 GET_INDUCTANCE                5 GET_VERSION                   6                               7
-        Unimplemented,                  DEVICE_GetVersion,              Undefined,                      Undefined,
+        Unimplemented,                  DEVICE_GetVersion,              DEVICE_get_fw_version,          Undefined,
      // 8 RETRIEVE_BUFFER               9 GET_HIGH_FREQUENCY            10 CLEAR_BUFFER                 11 SET_RGB1
         BUFFER_Retrieve,                Unimplemented,                  BUFFER_Clear,                   Removed,
      // 12 READ_PROGRAM_ADDRESS         13 WRITE_PROGRAM_ADDRESS        14 READ_DATA_ADDRESS            15 WRITE_DATA_ADDRESS
