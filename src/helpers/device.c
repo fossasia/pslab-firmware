89234547b0d6691c1cb99aeac3fa1c3e442152fe
Alexander Bessman <alexander@bessman.se>
2023-09-20 21:42:51 +0200

    Minor improvements

--- src/helpers/device.c
+++ src/helpers/device.c
@@ -8,24 +8,27 @@
 
 #define SEMVERS 3
 
-const uint8_t VERSION_HW[] = "PSLab V6"; /** Hardware version. **/
-union {
+static uint8_t const VERSION_HW[] = "PSLab V6"; /** Hardware version. **/
+static union {
     struct {
         uint8_t const major;
         uint8_t const minor;
         uint8_t const patch;
     };
     uint8_t const version[SEMVERS];
-} VERSION_FW = {{
+} const VERSION_FW = {{
     .major = 3,
     .minor = 0,
     .patch = 0
 }};
 
 response_t DEVICE_GetVersion(void) {
-    uint8_t i;
-    for (i = 0; i < sizeof(VERSION_HW) - 1; i++) UART1_Write(VERSION_HW[i]);
+    for (size_t i = 0; i < sizeof(VERSION_HW) - 1; ++i) {
+        UART1_Write(VERSION_HW[i]);
+    }
+
     UART1_Write('\n');
+
     return DO_NOT_BOTHER;
 }
 
@@ -33,11 +36,13 @@ response_t DEVICE_get_fw_version(void) {
     for (size_t i = 0; i < SEMVERS; ++i) {
         UART1_Write(VERSION_FW.version[i]);
     }
+
     return DO_NOT_BOTHER;
 }
 
 response_t DEVICE_Reset(void) {
     __asm__ volatile ("reset");
+
     return DO_NOT_BOTHER;
 }
 
@@ -51,7 +56,6 @@ response_t DEVICE_ReadRegisterData(void) {
 response_t DEVICE_WriteRegisterData(void) {
     uint16_t *address = (uint16_t *) (UART1_ReadInt() & 0xFFFF);
     uint16_t data = UART1_ReadInt();
-
     *address = data;
 
     return SUCCESS;
