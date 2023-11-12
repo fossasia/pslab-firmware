cd8193ff1c452946d77b124d83bba4c999640da0
Alexander Bessman <alexander@bessman.se>
2023-11-12 23:06:16 +0100

    Finish up sdcard read and write over serial

--- src/sdcard/sdcard.c
+++ src/sdcard/sdcard.c
@@ -5,16 +5,19 @@
 #include "../bus/uart/uart.h"
 #include "../commands.h"
 #include "fatfs/ff.h"
+#include "fatfs/ffconf.h"
+#include "../helpers/debug.h"
+#include "../registers/system/watchdog.h"
 
-#define BUF_SIZE 256
+#define BUF_SIZE FF_MIN_SS
 
 static FATFS drive;
 static FIL file;
-static char buffer[BUF_SIZE];
+static TCHAR buffer[BUF_SIZE];
 
-static void get_filename(void) {
-    for (uint8_t i = 0; i < BUF_SIZE; ++i) {
-        if (!(buffer[i] = UART1_Read())) {
+static void get_filename(TCHAR* const fn_buf, UINT const size) {
+    for (UINT i = 0; i < size; ++i) {
+        if (!(fn_buf[i] = UART1_Read())) {
             // Received null-terminator.
             return;
         }
@@ -23,115 +26,74 @@ static void get_filename(void) {
     // manual check for null-termination.
 }
 
-/// @brief Discard new errors if one has already happened.
-/// @param old Pointer to FRESULT.
-/// @param new New FRESULT.
-static void keep_first_error(FRESULT* old, FRESULT new) {
-    *old || (*old = new);
-}
-
 response_t SDCARD_write_file(void) {
-    struct inbytes {
-        char* const filename;
-        uint8_t const mode;
-        size_t const data_size;
-        uint8_t* const data;
-    } const inbytes = {
-        .filename = buffer,
-        .mode = UART1_Read(),
-        .data_size = UART1_read_u32(),
-        .data = buffer,
-    };
-    struct outbytes {
-        FRESULT result;
-        size_t bytes_written;
-    } outbytes = {0};
+    TCHAR* const filename = buffer;
+    get_filename(filename, BUF_SIZE);
+    BYTE const mode = UART1_Read();
+    DEBUG_write_u8(f_mount(&drive, "0:", 1));
+    // Host must wait for f_open before sending data.
+    UART1_Write(f_open(&file, filename, FA_WRITE | mode));
 
-
-    get_filename();
-    keep_first_error(&outbytes.result, f_mount(&drive, "0:", 1));
-    keep_first_error(
-        &outbytes.result,
-        f_open(&file, inbytes.filename, FA_WRITE | inbytes.mode)
-    );
-
-    UART1_Write(outbytes.result);
-
-    if (outbytes.result) {
-        return DO_NOT_BOTHER;
-    }
+    FSIZE_t data_size = UART1_read_u32();
+    FSIZE_t bytes_written = 0;
 
     for (
-        size_t block_size, remaining = inbytes.data_size;
+        FSIZE_t block_size, remaining = data_size;
         block_size = remaining > BUF_SIZE ? BUF_SIZE : remaining,
         remaining;
         remaining -= block_size
     ) {
-        for (size_t i = 0; i < block_size; ++i) {
-            inbytes.data[i] = UART1_Read();
+        for (UINT i = 0; i < block_size; ++i) {
+            buffer[i] = UART1_Read();
         }
 
-        uint8_t written = 0;
-        keep_first_error(
-            &outbytes.result,
-            f_write(&file, inbytes.data, block_size, &written)
-        );
-        outbytes.bytes_written += written;
+        WATCHDOG_TimerClear();
+        UINT written = 0;
+        // Host must wait for f_write before sending more data.
+        UART1_Write(f_write(&file, buffer, (UINT)block_size, &written));
+        bytes_written += written;
+        DEBUG_write_u32(remaining);
     }
 
-    keep_first_error(&outbytes.result, f_close(&file));
-    keep_first_error(&outbytes.result, f_mount(0, "0:", 0));
-
-    UART1_write_u32(outbytes.bytes_written);
-    UART1_Write(outbytes.result);
+    UART1_write_u32(bytes_written);
+    DEBUG_write_u8(f_close(&file));
+    DEBUG_write_u8(f_mount(0, "0:", 0));
 
     return DO_NOT_BOTHER;
 }
 
 response_t SDCARD_read_file(void) {
-    struct inbytes {
-        char* const filename;
-    } const inbytes = {
-        .filename = buffer,
-    };
-    struct outbytes {
-        FRESULT result;
-        size_t bytes_read;
-        char* data;
-    } outbytes = {
-        .result = 0,
-        .bytes_read = 0,
-        .data = buffer,
-    };
+    TCHAR* const filename = buffer;
+    get_filename(filename, BUF_SIZE);
+    DEBUG_write_u8(f_mount(&drive, "0:", 1));
+    DEBUG_write_u8(f_open(&file, filename, FA_READ));
 
-    get_filename();
-    keep_first_error(&outbytes.result, f_mount(&drive, "0:", 1));
-    keep_first_error(&outbytes.result, f_open(&file, inbytes.filename, FA_READ));
-    FILINFO info = {0};
-    keep_first_error(&outbytes.result, f_stat(inbytes.filename, &info));
+    FILINFO info = {0, 0, 0, 0, {0}};
+    DEBUG_write_u8(f_stat(filename, &info));
     UART1_write_u32(info.fsize);
+    FSIZE_t bytes_read = 0;
 
     for (
-        size_t block_size, remaining = info.fsize;
+        FSIZE_t block_size, remaining = info.fsize;
         block_size = remaining > BUF_SIZE ? BUF_SIZE : remaining,
         remaining;
         remaining -= block_size
     ) {
-        uint8_t read = 0;
-        keep_first_error(&outbytes.result, f_read(&file, &buffer, block_size, &read));
+        WATCHDOG_TimerClear();
+        UINT read = 0;
+        f_read(&file, &buffer, (UINT)block_size, &read);
+        bytes_read += read;
 
-        for (size_t i = 0; i < block_size; ++i) {
+        for (UINT i = 0; i < block_size; ++i) {
             UART1_Write(buffer[i]);
         }
 
-        outbytes.bytes_read += read;
+        DEBUG_write_u32(remaining);
     }
 
-    keep_first_error(&outbytes.result, f_close(&file));
-    keep_first_error(&outbytes.result, f_mount(0, "0:", 0));
-
-    UART1_write_u32(outbytes.bytes_read);
-    UART1_Write(outbytes.result);
+    UART1_write_u32(bytes_read);
+    DEBUG_write_u8(f_close(&file));
+    DEBUG_write_u8(f_mount(0, "0:", 0));
 
     return DO_NOT_BOTHER;
 }
--- src/sdcard/sdcard.h
+++ src/sdcard/sdcard.h
@@ -6,26 +6,43 @@
 /**
  * @brief Write data to a file on the SD-card.
  *
- * @param filename char[256]
- *        Null-terminated filename, at most 256 characters long (including
- *        null-terminator).
- * @param mode uint8_t
- *        Access mode. A combination of:
- *        OPEN_EXISTING = 0x00
- *        CREATE_NEW = 0x04
- *        CREATE_ALWAYS = 0x08
- *        OPEN_ALWAYS = 0x10
- *        OPEN_APPEND = 0x30
- * @param data_size uint32_t
- *        Number of bytes to write to the file.
- * @param data uint8_t[data_size]
- *        Data to write to the SD-card.
+ * @details UART transaction order:
+ *             Rx[1-12]    filename
+ *             Rx[1]       mode
+ *     <DEBUG> Tx[1]       result_mount
+ *             Tx[1]       result_open
+ *             Rx[4]       data_size
+ *             loop until entire file written:
+ *                 Rx[512] data
+ *                 Tx[1]   result_write
+ *     <DEBUG>     Tx[4]   remaining_bytes
+ *             Tx[4]       total_bytes
+ *     <DEBUG> Tx[1]       result_close
+ *     <DEBUG> Tx[1]       result_unmount
  *
- * @return bytes_written uint16_t
- *         Number of bytes which were actually written to the SD-card. This
- *         should be equal to `data_size` if all went well.
- * @return result uint8_t
- *         FatFS result code.
+ * @param filename TCHAR[12]
+ *     Null-terminated 8.3 filename. 8.3 filenames are limited to at most
+ *     eight characters (after any directory specifier), followed
+ *     optionally by a filename extension consisting of a period and at
+ *     most three further characters.
+ * @param mode BYTE
+ *     Access mode. A combination of:
+ *     OPEN_EXISTING = 0x00
+ *     CREATE_NEW = 0x04
+ *     CREATE_ALWAYS = 0x08
+ *     OPEN_ALWAYS = 0x10
+ *     OPEN_APPEND = 0x30
+ * @param data_size FSIZE_t
+ *     Number of bytes to write to the file.
+ * @param data TCHAR[512] [repeats until data_size written]
+ *     Data to write to the SD-card.
+ *
+ * @return result_open FResult
+ * @return result_write FResult [repeats once per write]
+ * @return bytes_written FSIZE_t
+ *     Number of bytes which were actually written to the SD-card. This
+ *     should be equal to `data_size` if all went well.
+ *
  * @return DO_NOT_BOTHER
  */
 response_t SDCARD_write_file(void);
@@ -33,20 +50,33 @@ response_t SDCARD_write_file(void);
 /**
  * @brief Read data from a file on the SD-card.
  *
- * @param filename char[256]
- *        Null-terminated filename, at most 256 characters long (including
- *        null-terminator).
- *
- * @return file_size uint32_t
+ * @details UART transaction order:
+ *             Rx[1-12]    filename
+ *     <DEBUG> Tx[1]       result_mount
+ *     <DEBUG> Tx[1]       result_open
+ *     <DEBUG> Tx[1]       result_stat
+ *             Tx[4]       data_size
+ *             loop until entire file read:
+ *                 Tx[512] data
+ *     <DEBUG>     Tx[4]   remaining_bytes
+ *             Tx[4]       total_bytes
+ *     <DEBUG> Tx[1]       result_close
+ *     <DEBUG> Tx[1]       result_unmount
+ * 
+ * @param filename TCHAR[12]
+ *     Null-terminated 8.3 filename. 8.3 filenames are limited to at most
+ *     eight characters (after any directory specifier), followed
+ *     optionally by a filename extension consisting of a period and at
+ *     most three further characters.
+ * @return data_size FSIZE_t
  *         Size of the file.
- * @return data uint8_t[file_size]
- *         Data read from the SD-card.
- * @return bytes_read uint32_t
- *         Number of bytes which were actually read from the SD-card. This
- *         should be equal to `file_size` if all went well. If it is not
- *         equal to `file_size`, any bytes beyond `bytes_read` are invalid.
- * @return result uint8_t
- *         FatFS result code.
+ * @return data TCHAR[512] [repeats until data_size read]
+ *     Data read from the SD-card.
+ * @return total_bytes FSIZE_t
+ *     Number of bytes which were actually read from the SD-card. This
+ *     should be equal to `file_size` if all went well. If it is not
+ *     equal to `file_size`, any bytes beyond `bytes_read` are invalid.
+ *
  * @return DO_NOT_BOTHER
  */
 response_t SDCARD_read_file(void);
