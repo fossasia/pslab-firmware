531606edf0219778c189a1383a894124e4a16514
Hrushi20 <hrushikesh20thegreat@gmail.com>
2023-11-14 13:30:15 +0530

    rtc_settime, rtc_gettime functions added
    Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

--- src/helpers/rtc.c
+++ src/helpers/rtc.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <time.h>
 
 #include "../bus/i2c/i2c.h"
 #include "../bus/uart/uart.h"
@@ -8,18 +9,50 @@
 #include "light.h"
 #include "rtc.h"
 
+uint8_t bcd(uint8_t data){
+    uint8_t left,right;
+    uint8_t bcd = data;
+
+    if(data >= 10){
+        left = left / 10;
+        right = right % 10;
+        bcd = (left << 4) | right;
+    }
+    return bcd;
+}
+
 response_t RTC_SetTime(void) {
-    
+
+    const long unix_timestamp = (long) UART1_read_u32();
+    struct tm *tm_info;
+
+    tm_info = gmtime(&unix_timestamp);
+    uint8_t sec = tm_info->tm_sec;
+    uint8_t min = tm_info->tm_min;
+    uint8_t hours = tm_info->tm_hour;
+    uint8_t day = tm_info->tm_wday + 1;
+    uint8_t date = tm_info->tm_mday;
+    uint8_t month = tm_info->tm_mon + 1;
+
+    if(tm_info->tm_year < 2000 || tm_info->tm_year > 2099)
+        return ARGUMENT_ERROR;
+
+    uint8_t year = (tm_info->tm_year + 1900) % 100;
+
+    if(sec == 60)
+        sec = 0;
+
+    // Default 24 hrs format.
     uint8_t buffer[9];
     buffer[0] = DS1307_DATA_REG_SECONDS;
-    buffer[1] = UART1_Read() & 0x7F;  // seconds
-    buffer[2] = UART1_Read();         // minutes
-    buffer[3] = UART1_Read();         // hours
-    buffer[4] = UART1_Read();         // day
-    buffer[5] = UART1_Read();         // date
-    buffer[6] = UART1_Read();         // month
-    buffer[7] = UART1_Read();         // year
-    buffer[8] = UART1_Read();         // control
+    buffer[1] = bcd(sec) & ~(1<<7)                      // seconds
+    buffer[2] = bcd(min)                                // minutes
+    buffer[3] = (bcd(hours) & (1<<5))                   // hours (hrs format)
+    buffer[4] = bcd(day)                                // day
+    buffer[5] = bcd(date)                               // date
+    buffer[6] = bcd(month)                              // month
+    buffer[7] = bcd(year)                               // year
+    buffer[8] = 0                                       // control
 
     I2C_InitializeIfNot(I2C_BAUD_RATE_100KHZ, I2C_ENABLE_INTERRUPTS);
 
@@ -40,12 +73,23 @@ response_t RTC_SetDigit(void) {
 response_t RTC_GetTime(void) {
 
     uint8_t buffer[7];
+    struct tm tm_info;
 
     I2C_InitializeIfNot(I2C_BAUD_RATE_100KHZ, I2C_ENABLE_INTERRUPTS);
 
     if(I2C_BulkRead(DS1307_DATA_REG_SECONDS, DS1307_I2C_DEVICE_ADDRESS, buffer, 7) == SUCCESS) {
-        uint8_t i;
-        for (i = 0; i < sizeof(buffer); i++) UART1_Write(buffer[i]);
+        tm_info.tm_sec = buffer[0];
+        tm_info.tm_min = buffer[1];
+        tm_info.tm_hour = buffer[2];
+        tm_info.tm_wday = buffer[3] - 1;
+        tm_info.tm_mday = buffer[4];
+        tm_info.tm_mon = buffer[5] - 1;
+        tm_info.tm_year = 2000 + buffer[6];
+
+        tm_info.tm_sec = tm_info.tm_sec & ~(1 << 7);
+
+        uint32_t unix_timestamp = (uint32_t) mktime(tm_info);
+        UART1_write_u32(unix_timestamp);
     } else return FAILED;
 
     return SUCCESS;
