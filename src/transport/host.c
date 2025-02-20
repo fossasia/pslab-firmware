9f11f2ef3a708806e2469936677046dbbf269fd0
Alexander Bessman <alexander.bessman@gmail.com>
2025-02-20 15:46:29 +0100

    Add HOST_{read,write}_{u8,u16,u32} convenience functions

--- src/transport/host.c
+++ src/transport/host.c
@@ -27,11 +27,41 @@ enum Status HOST_read(
     }
 }
 
+enum Status HOST_read_u8(uint8_t *const u8)
+{
+    return HOST_read(u8, sizeof(uint8_t));
+}
+
+enum Status HOST_read_u16(uint16_t *const u16)
+{
+    return HOST_read((uint8_t *const)u16, sizeof(uint16_t));
+}
+
+enum Status HOST_read_u32(uint32_t *const u32)
+{
+    return HOST_read((uint8_t *const)u32, sizeof(uint32_t));
+}
+
 enum Status HOST_write(
     uint8_t const *const buffer,
     uint16_t const size
 ) {
     return UART_write(PSLAB_HOST, buffer, size);
+}
+
+enum Status HOST_write_u8(uint8_t const u8)
+{
+    return HOST_write(&u8, sizeof(uint8_t));
+}
+
+enum Status HOST_write_u16(uint16_t const u16)
+{
+    return HOST_write((uint8_t const *const)&u16, sizeof(uint16_t));
+}
+
+enum Status HOST_write_u32(uint32_t const u32)
+{
+    return HOST_write((uint8_t const *const)&u32, sizeof(uint32_t));
 }
 
 enum Status HOST_flush_rx(void)
--- src/transport/host.h
+++ src/transport/host.h
@@ -6,7 +6,13 @@ enum Status HOST_read(
     uint16_t size,
     uint16_t *num_bytes_read
 );
+enum Status HOST_read_u8(uint8_t *u8);
+enum Status HOST_read_u16(uint16_t *u16);
+enum Status HOST_read_u32(uint32_t *u32);
 enum Status HOST_write(uint8_t const *buffer, uint16_t size);
+enum Status HOST_write_u8(uint8_t u8);
+enum Status HOST_write_u16(uint16_t u16);
+enum Status HOST_write_u32(uint32_t u32);
 enum Status HOST_flush_rx(void);
 
 #endif // _PSLAB_HOST
