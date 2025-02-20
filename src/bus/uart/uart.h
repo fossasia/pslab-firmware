21c247e48f1a524c518b6d5bd77ae6ef68656052
Alexander Bessman <alexander.bessman@gmail.com>
2025-02-20 15:44:39 +0100

    Remove leftovers of old UART driver

--- src/bus/uart/uart.h
+++ src/bus/uart/uart.h
@@ -22,197 +22,98 @@ typedef enum
  *
  * @param select Either U1SELECT or U2SELECT.
  */
-void UART_Initialize(const EUxSelect select);
+void UART_initialize(EUxSelect const select);
 
-enum Status UART_read(
-    EUxSelect select,
-    uint8_t *buffer,
-    uint16_t buffer_size,
-    uint16_t *num_bytes_read
-);
+/**
+ * @brief Read bytes from UARTx bus into buffer.
+ *
+ * @param select U1SELECT or U2SELECT.
+ * @param buffer Pointer to writable uint8_t array.
+ * @param size Size of buffer.
+ * @return enum Status
+ */
+enum Status UART_read(EUxSelect select, uint8_t *buffer, uint16_t size);
+
+/**
+ * @brief Write bytes to UARTx from buffer.
+ *
+ * @param select U1SELECT or U2SELECT.
+ * @param buffer Pointer to uint8_t array.
+ * @param size Size of buffer.
+ * @return enum Status
+ */
 enum Status UART_write(EUxSelect select, uint8_t const *buffer, uint16_t size);
+
+/**
+ * @brief Consume any incoming data until bus is idle.
+ *
+ * @param select U1SELECT or U2SELECT.
+ * @return enum Status
+ */
 enum Status UART_flush_rx(EUxSelect select);
 
 /**
- * @brief Read a byte of data from UARTx.
- * @pre
- *   UART_Initialize() should have been called for UARTx before
- *   calling this function.
- *   UART_IsRxReady() should be used to check if there is incoming data before
- *   calling this function. This function will block for up to 1 ms while
- *   waiting for data to become available on UxRXREG.
+ * @brief Read a one-byte integer value from UART2 and write it to UART1.
  *
- * @param select Either U1SELECT or U2SELECT.
- * @return
- *   Eight bits of data. The return value is zero case of bus error or timeout.
- *   There is no indication of whether a return value of zero is valid or not.
+ * @return NACK
  */
-uint8_t UART_Read(const EUxSelect select);
+response_t UART2_read_u8(void);
 
 /**
- * @brief Convenience function, equivalent to UART_Read(U1SELECT).
- */
-uint8_t UART1_Read(void);
-
-/**
- * @brief Read a word of data from UARTx.
+ * @brief Read a two-byte integer value from UART2 and write it to UART1.
  *
- * The word consists of two bytes, lsb first.
+ * @return NACK
+ */
+response_t UART2_read_u16(void);
+
+/**
+ * @brief Read a one-byte integer value from UART1 and write it to UART2.
  *
- * @pre
- *   UART_Initialize() should have been called for UARTx before
- *   calling this function.
- *   UART_IsRxReady() should be used to check if there is incoming data before
- *   calling this function. This function will block for up to 2 ms while
- *   waiting for data to become available on UxRXREG.
+ * @return NACK
+ */
+response_t UART2_write_u8(void);
+
+/**
+ * @brief Read a two-byte integer value from UART1 and write it to UART2.
  *
- * @param select Either U1SELECT or U2SELECT.
- * @return
- *   16 bits of data. The return value is zero case of bus error or timeout.
- *   Note that it is possible for one of the bytes to be valid, while the
- *   other may be invalid due to an error.
-*/
-uint16_t UART_ReadInt(const EUxSelect select);
-
-/**
- * @brief Convenience function, equivalent to UART_ReadInt(U1SELECT).
+ * @return NACK
  */
-uint16_t UART1_ReadInt(void);
+response_t UART2_write_u16(void);
 
 /**
- * @brief Read a uint32_t from UARTx.
+ * @brief Query whether there is data available to read from UART2.
  *
- * @pre
- *   UART_Initialize() must have been called for UARTx before
- *   calling this function.
- *   UART_IsRxReady() should be used to check if there is incoming data before
- *   calling this function. This function will block for up to 4 ms while
- *   waiting for data to become available on UxRXREG.
- *
- * @param select Either U1SELECT or U2SELECT.
- * @return uint32_t
+ * @return rx_ready
+ *   True if at least one more byte can be read from UART2.
+ * @return NACK
  */
-uint32_t UART_read_u32(EUxSelect const select);
-
-/**
- * @brief Convenience function, equivalent to UART_read_u32(U1SELECT).
- */
-uint32_t UART1_read_u32(void);
-
-/**
- * @brief Writes a byte of data to the UARTx.
- * @pre
- *   UART2_Initialize() should have been called before calling this function.
- *
- * @param select Either U1SELECT or U2SELECT.
- * @param txData Data byte to write to UARTx.
- */
-void UART_Write(const EUxSelect select, uint8_t txData);
-
-/**
- * @brief Convenience function, equivalent to UART_Write(U1SELECT, data).
- */
-uint16_t UART1_ReadInt(void);
-void UART1_Write(uint8_t txData);
-
-/**
- * @brief Writes a word of data to the UARTx.
- *
- * The word consists of two bytes, lsb first.
- *
- * @pre
- *   UART2_Initialize() should have been called before calling this function.
- *
- * @param select Either U1SELECT or U2SELECT.
- * @param txData Data word to write to UARTx.
- */
-void UART_WriteInt(const EUxSelect select, uint16_t txData);
-
-/**
- * @brief Convenience function, equivalent to UART_WriteInt(U1SELECT, data).
- */
-void UART1_WriteInt(uint16_t txData);
-
-/**
- * @brief Writes a uint32_t to UARTx.
- *
- * @pre
- *   UART2_Initialize() should have been called before calling this function.
- *
- * @param select Either U1SELECT or U2SELECT.
- * @param txData Data word to write to UARTx.
- */
-void UART_write_u32(EUxSelect const select, uint32_t const txdata);
-
-/**
- * @brief Convenience function, equivalent to UART_write_u32(U1SELECT, data).
- */
-void UART1_write_u32(uint32_t const txdata);
-
-/**
- * @brief Indicates whether there is data available to read.
- * @return True if at least one byte can be read.
- */
-bool UART_IsRxReady(const EUxSelect select);
-
-/**
- * @brief Discard incoming serial traffic.
- *
- * Reads the RX buffer until no more data is available.
- */
-void UART_ClearBuffer(const EUxSelect select);
-
-/**
- * @brief Read a byte from UART2 and write it to UART1.
- * @return DO_NOT_BOTHER
- */
-response_t UART2_Read(void);
-
-/**
- * @brief Read a word from UART2 and write it to UART1.
- * @return DO_NOT_BOTHER
- */
-response_t UART2_ReadWord(void);
-
-/**
- * @brief Read a byte from UART1 and write it to UART2.
- * @param TxData One byte of data to write to UART2.
- * @return DO_NOT_BOTHER
- */
-response_t UART2_Write(void);
-
-/**
- * @brief Read a word from UART1 and write it to UART2.
- * @param TxData Two bytes of data, lsb first, to write to UART2.
- * @return DO_NOT_BOTHER
- */
-response_t UART2_WriteWord(void);
-
-/**
- * @brief Check if data is available on UART2.
- *
- * Writes true (1) to UART1 if data is available, false (0) otherwise.
- *
- * @return DO_NOT_BOTHER
- */
-response_t UART2_RxReady(void);
+response_t UART2_rx_ready(void);
 
 /**
  * @brief Set baudrate of UART2.
- * @param baud A BRGVAL, typically one of EBaudrate.
- * @return SUCCESS
- */
-response_t UART2_SetBaud(void);
-
-/**
- * @brief Set stop bits and parity of UART2.
- * @param mode
- *   One byte, where bit 0 is the stop bit configuration value, and bits two
- *   and three are the parity configuration value.
+ *
+ * @param brgval
+ *   BRGVAL to set on UART2.
  *
  * @return SUCCESS
  */
-response_t UART2_SetMode(void);
+response_t UART2_set_baud(void);
+
+/**
+ * @brief Set UART2 stop bit and parity.
+ *
+ * @param mode
+ *   Bit 0 sets UART2 stop bit:
+ *   0: One stop bit
+ *   1: Two stop bits
+ *   Bits 1-2 set UART2 parity:
+ *   0: No parity
+ *   1: Even parity
+ *   2: Odd parity
+ *
+ * @return response_t
+ */
+response_t UART2_set_mode(void);
 
 /**
  * @brief Send all traffic from UART1 to UART2 and vice versa.
