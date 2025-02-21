4ed3cb531f36b10498ef08ea56f5d3c8d47e30c5
Alexander Bessman <alexander@bessman.se>
2025-02-21 15:21:40 +0100

    Update sample buffer for new com arch

--- src/helpers/buffer.c
+++ src/helpers/buffer.c
@@ -7,68 +7,116 @@
 // Space in memory to store data.
 uint16_t volatile __attribute__((section(".adc_buffer"), far)) BUFFER[BUFFER_SIZE];
 
-response_t BUFFER_Retrieve(void) {
-    
-    uint16_t volatile* idx = &BUFFER[UART1_ReadInt()];
-    uint16_t volatile* end = idx + UART1_ReadInt();
-
-    LED_SetLow();
-    while (idx != end) UART1_WriteInt(*(idx++));
-    LED_SetHigh();
-    
-    return SUCCESS;
-}
-
-response_t BUFFER_FetchInt(void) {
-    
-    uint16_t counter = UART1_ReadInt();
-    uint8_t channel = UART1_Read();
-    
-    LED_SetLow();
-    uint16_t i;
-    for (i = 0; i < counter; i++) {
-        UART1_WriteInt(BUFFER[i + channel * (BUFFER_SIZE / 4)]);
+static enum Status read(
+    uint16_t const start,
+    uint16_t const end,
+    uint8_t volatile **const out_samples,
+    uint16_t *const out_samples_size
+) {
+    if (start > end) {
+        return E_BAD_ARGUMENT;
     }
-    LED_SetHigh();
-    
-    return SUCCESS;
-}
 
-response_t BUFFER_FetchLong(void) {
-    
-    uint16_t counter = UART1_ReadInt();
-    uint8_t channel = UART1_Read();
-    
-    LED_SetLow();
-    uint16_t i;
-    for (i = 0; i < counter; i++) {
-        UART1_WriteInt(BUFFER[i + 2 * channel * (BUFFER_SIZE / 4)]);
-        UART1_WriteInt(BUFFER[i + (2 * channel + 1) * (BUFFER_SIZE / 4)]);
+    if (end > BUFFER_SIZE) {
+        return E_BAD_ARGUMENT;
     }
-    LED_SetHigh();
-    
-    return SUCCESS;
+
+    *out_samples = (uint8_t volatile *const)(&BUFFER + start);
+    *out_samples_size = end - start;
+    return E_OK;
 }
 
-response_t BUFFER_Fill(void) {
-    
-    uint16_t start = UART1_ReadInt();
-    uint16_t end = UART1_ReadInt();
-    
-    uint16_t i;
-    for (i = start; i < start + end; i++) {
-        BUFFER[i] = UART1_ReadInt();
+enum Status BUFFER_read(
+    uint8_t const *const args,
+    uint16_t const args_size,
+    uint8_t volatile **const rets,
+    uint16_t *const rets_size
+) {
+    uint16_t start = 0;
+    uint16_t end = 0;
+
+    if (args_size != (sizeof(start) + sizeof(end))) {
+        return E_BAD_ARGSIZE;
     }
-    
-    return SUCCESS;
+
+    start = ((uint16_t *)args)[0];
+    end = ((uint16_t *)args)[1];
+    return read(start, end, rets, rets_size);
 }
 
-response_t BUFFER_Clear(void) {
-    
-    uint16_t start = UART1_ReadInt();
-    uint16_t end = UART1_ReadInt();
-    
+enum Status BUFFER_read_from_channel(
+    uint8_t const *const args,
+    uint16_t const args_size,
+    uint8_t volatile **const rets,
+    uint16_t *const rets_size
+) {
+    uint16_t counter = 0;
+    uint8_t channel = 0;
+
+    if (args_size != (sizeof(counter) + sizeof(channel))) {
+        return E_BAD_ARGSIZE;
+    }
+
+    counter = ((uint16_t *)args)[0];
+    channel = args[2];
+    uint8_t const num_channels = 4;
+    uint16_t const start = channel * (BUFFER_SIZE / num_channels);
+    uint16_t const end = start + counter;
+    return read(start, end, rets, rets_size);
+}
+    }
+}
+
+enum Status BUFFER_write(
+    uint8_t const *const args,
+    uint16_t const args_size,
+    uint8_t volatile **const rets,
+    uint16_t *const rets_size
+) {
+
+    uint16_t start = 0;
+    uint16_t count = 0;
+
+    if (args_size != (sizeof(start) + sizeof(count))) {
+        return E_BAD_ARGSIZE;
+    }
+
+    if (start + count > BUFFER_SIZE) {
+        return E_BAD_ARGUMENT;
+    }
+
+    start = ((uint16_t *)args)[0];
+    count = ((uint16_t *)args)[1];
+    *rets = (uint8_t volatile *const)(&BUFFER + start);
+    *rets_size = count;
+    return E_OK;
+}
+
+enum Status BUFFER_clear(
+    uint8_t const *const args,
+    uint16_t const args_size,
+    __attribute__ ((unused)) uint8_t **const rets,
+    __attribute__ ((unused)) uint16_t *const rets_size
+) {
+
+    uint16_t start = 0;
+    uint16_t end = 0;
+
+    if (args_size != (sizeof(start) + sizeof(end))) {
+        return E_BAD_ARGSIZE;
+    }
+
+    start = ((uint16_t *)args)[0];
+    end = ((uint16_t *)args)[1];
+
+    if (start > end) {
+        return E_BAD_ARGUMENT;
+    }
+
+    if (end > BUFFER_SIZE) {
+        return E_BAD_ARGUMENT;
+    }
+
     memset((void *) &BUFFER[start], 0, (end - start) * sizeof(int));
-    
-    return SUCCESS;
+    return E_OK;
 }
--- src/helpers/buffer.h
+++ src/helpers/buffer.h
@@ -10,24 +10,29 @@ extern "C" {
 #endif
 
     extern uint16_t volatile __attribute__((section(".adc_buffer"), far)) BUFFER[BUFFER_SIZE];
-    
+
     /**
     * @brief Send buffer contents.
-    * 
+    *
     * @description
     * This command function takes two arguments over serial:
     * 1. The starting index in the buffer from which to send values.
     * 2. The number of values to be sent.
     * It returns the requested data over serial.
     * It sends an acknowledge byte (SUCCESS)
-    * 
+    *
     * @return SUCCESS
     */
-    response_t BUFFER_Retrieve(void);
-    
+    enum Status BUFFER_read(
+        uint8_t const *args,
+        uint16_t args_size,
+        uint8_t volatile **rets,
+        uint16_t *rets_size
+    );
+
     /**
     * @brief Send buffer content as integers.
-    * 
+    *
     * @description
     * This command function takes two arguments over serial:
     * 1. The starting index in the buffer from which to send values.
@@ -35,44 +40,40 @@ extern "C" {
     *
     * It returns the requested data over serial.
     * It sends an acknowledge byte (SUCCESS)
-    * 
-    * @return SUCCESS
-    */
-    response_t BUFFER_FetchInt(void);
-    
-    /**
-    * @brief Send buffer content as longs.
-    * 
-    * @description
-    * This command function takes two arguments over serial:
-    * 1. The starting index in the buffer from which to send values.
-    * 2. The number of values to be sent.
     *
-    * It returns the requested data over serial.
-    * It sends an acknowledge byte (SUCCESS)
-    * 
     * @return SUCCESS
     */
-    response_t BUFFER_FetchLong(void);
+    enum Status BUFFER_read_from_channel(
+        uint8_t const *args,
+        uint16_t args_size,
+        uint8_t volatile **rets,
+        uint16_t *rets_size
+    );
 
     /**
     * @brief Populate BUFFER array
-    * 
+    *
     * @description
     * This command function takes two arguments over serial:
     * 1. The starting index in the buffer from which to fill values.
     * 2. The number of values to be sent.
-    * 
+    *
     * It does not return anything over serial.
     * It sends an acknowledge byte (SUCCESS)
-    * 
+    *
     * @return SUCCESS
     */
-    response_t BUFFER_Fill(void);
-    
+    enum Status BUFFER_write(
+        uint8_t const *args,
+        uint16_t args_size,
+        uint8_t volatile **rets,
+        uint16_t *rets_size
+    );
+
+
     /**
     * @brief Clear BUFFER array
-    * 
+    *
     * @description
     * This command function takes two arguments over serial:
     * 1. The starting index in the buffer from which it should be emptied
@@ -80,10 +81,15 @@ extern "C" {
     *
     * It does not return anything over serial.
     * It sends an acknowledge byte (SUCCESS)
-    * 
+    *
     * @return SUCCESS
     */
-    response_t BUFFER_Clear(void);
+    enum Status BUFFER_clear(
+        uint8_t const *args,
+        uint16_t args_size,
+        uint8_t **const rets,
+        uint16_t *rets_size
+    );
 
 #ifdef	__cplusplus
 }
