a8ff4b65cb1fcdaa6726abe770bdcc266d22b3f1
Alexander Bessman <alexander.bessman@gmail.com>
2025-02-24 14:42:34 +0100

    Update rtc module

--- src/helpers/rtc.c
+++ src/helpers/rtc.c
@@ -2,12 +2,24 @@
 #include <time.h>
 
 #include "../bus/i2c/i2c.h"
-#include "../bus/uart/uart.h"
 #include "../commands.h"
 #include "../registers/system/pin_manager.h"
 #include "delay.h"
 #include "light.h"
 #include "rtc.h"
+
+#define DS1307_I2C_DEVICE_ADDRESS 0x68
+
+typedef enum {
+    DS1307_DATA_REG_SECONDS, // Clock Halt (1) | 10 Seconds (3) | Seconds (4)
+    DS1307_DATA_REG_MINUTES, // XX (1) | 10 Minutes (3) | Minutes (4)
+    DS1307_DATA_REG_HOURS,   // XX (1) | 12/24 | AM/PM | 10h | Hours (4)
+    DS1307_DATA_REG_DAYS,    // XX (5) | Day (3)
+    DS1307_DATA_REG_DATES,   // XX (2) | 10 Dates (2) | Dates (4)
+    DS1307_DATA_REG_MONTHS,  // XX (3) | 10 Months (1) | Month (4)
+    DS1307_DATA_REG_YEARS,   // 10 Years (4) | Year (4) [00 - 99]
+    DS1307_DATA_REG_CONTROL  // OUT (1) | XX (2) | SQWE (1) | XX (2) | RS1:0 (2)
+} DS1307_DATA_REG;
 
 // This bit is used to enable the oscillator. When negated, the oscillator is disabled.
 static uint8_t const oscillator_enable = 0x7F;
@@ -31,13 +43,9 @@ static uint8_t bcd_to_data(uint8_t bcd){
     return left + right;
 }
 
-response_t RTC_SetTime(uint32_t const * const unix_timestamp) {
-
-    time_t timestamp = (time_t) *unix_timestamp;
-    struct tm *tm_info;
-
-    tm_info = gmtime(&timestamp);
-    uint8_t sec = tm_info->tm_sec;
+enum Status RTC_set_time(time_t const *const unix_timestamp) {
+    struct tm *const tm_info = gmtime(unix_timestamp);
+    uint8_t sec = tm_info->tm_sec % 60;
     uint8_t min = tm_info->tm_min;
     uint8_t hours = tm_info->tm_hour;
     uint8_t day = tm_info->tm_wday + 1;
@@ -45,8 +53,6 @@ response_t RTC_SetTime(uint32_t const * const unix_timestamp) {
     uint8_t month = tm_info->tm_mon + 1;
     uint8_t year = tm_info->tm_year % 100; // Tm_year starts from 00 which means(1900).
                                            // ds1307 only stores the last 2 digits.
-    if(sec == 60)
-        sec = 0;
 
     // Default 24 hrs format.
     uint8_t buffer[9];
@@ -65,71 +71,60 @@ response_t RTC_SetTime(uint32_t const * const unix_timestamp) {
     return I2C_BulkWrite(buffer, 9, DS1307_I2C_DEVICE_ADDRESS);
 }
 
-response_t RTC_CmdSetTime(void) {
-    uint32_t unix_timestamp = UART1_read_u32();
+enum Status RTC_cmd_set_time(
+    uint8_t const *args,
+    uint16_t const args_size,
+    __attribute__ ((unused)) uint8_t **rets,
+    __attribute__ ((unused)) uint16_t *rets_size
+) {
+    time_t unix_timestamp = 0;
 
-    response_t res = RTC_SetTime(&unix_timestamp);
+    if (args_size != sizeof(unix_timestamp)) {
+        return E_BAD_ARGSIZE;
+    }
+
+    unix_timestamp = *(time_t *)args;
+    enum Status res = RTC_set_time(&unix_timestamp);
     return res;
 }
 
-response_t RTC_SetDigit(void) {
-
-    uint8_t buffer[2];
-    buffer[0] = UART1_Read();  // register address
-    buffer[1] = UART1_Read();  // data
-
-    I2C_InitializeIfNot(I2C_BAUD_RATE_100KHZ, I2C_ENABLE_INTERRUPTS);
-
-    return I2C_BulkWrite(buffer, 2, DS1307_I2C_DEVICE_ADDRESS);
-}
-
-response_t RTC_GetTime(uint32_t* unix_timestamp) {
-
-    uint8_t buffer[7];
-    struct tm tm_info;
+enum Status RTC_get_time(time_t *const unix_timestamp) {
+    uint8_t buffer[7] = {0};
+    struct tm tm_info = {0};
+    enum Status status = E_OK;
 
     I2C_InitializeIfNot(I2C_BAUD_RATE_100KHZ, I2C_ENABLE_INTERRUPTS);
+    status = I2C_BulkRead(
+        DS1307_DATA_REG_SECONDS,
+        DS1307_I2C_DEVICE_ADDRESS,
+        buffer,
+        sizeof(buffer)
+    );
 
-    if(I2C_BulkRead(DS1307_DATA_REG_SECONDS, DS1307_I2C_DEVICE_ADDRESS, buffer, 7) == SUCCESS) {
+    // Need to convert from bcd to int.
+    tm_info.tm_sec = bcd_to_data(buffer[0]);
+    tm_info.tm_min = bcd_to_data(buffer[1]);
+    tm_info.tm_hour = bcd_to_data(buffer[2]);
+    tm_info.tm_wday = bcd_to_data(buffer[3]) - 1;
+    tm_info.tm_mday = bcd_to_data(buffer[4]);
+    tm_info.tm_mon = bcd_to_data(buffer[5]) - 1;
+    tm_info.tm_year = 100 + bcd_to_data(buffer[6]); // 100 means for year 2000 (2000 - 1900)
+    tm_info.tm_sec = tm_info.tm_sec & oscillator_enable;
 
-        // Need to convert from bcd to int.
-        tm_info.tm_sec = bcd_to_data(buffer[0]);
-        tm_info.tm_min = bcd_to_data(buffer[1]);
-        tm_info.tm_hour = bcd_to_data(buffer[2]);
-        tm_info.tm_wday = bcd_to_data(buffer[3]) - 1;
-        tm_info.tm_mday = bcd_to_data(buffer[4]);
-        tm_info.tm_mon = bcd_to_data(buffer[5]) - 1;
-        tm_info.tm_year = 100 + bcd_to_data(buffer[6]); // 100 means for year 2000 (2000 - 1900)
-
-        tm_info.tm_sec = tm_info.tm_sec & oscillator_enable;
-
-        uint32_t timestamp = (uint32_t) mktime(&tm_info);
-        *unix_timestamp = timestamp;
-    } else return FAILED;
-
-    return SUCCESS;
+    *unix_timestamp = mktime(&tm_info);
+    return status;
 }
 
-response_t RTC_CmdGetTime(void){
-    uint32_t unix_timestamp;
-    response_t res = RTC_GetTime(&unix_timestamp);
-
-    // What if error occurs here, Returns fail.
-    UART1_write_u32(unix_timestamp);
-    return res;
-}
-
-response_t RTC_GetDigit(void) {
-
-    uint8_t buffer[1];
-    uint8_t reg = UART1_Read();  // register address
-    uint8_t *pR = &reg;
-
-    I2C_InitializeIfNot(I2C_BAUD_RATE_100KHZ, I2C_ENABLE_INTERRUPTS);
-
-    if(I2C_BulkRead(pR, DS1307_I2C_DEVICE_ADDRESS, buffer, 1) == SUCCESS) {
-        UART1_Write(buffer[0]);
-    } else return FAILED;
-
-    return SUCCESS;
+enum Status RTC_cmd_get_time(
+    uint8_t *args,
+    __attribute__ ((unused)) uint16_t const args_size,
+    uint8_t **rets,
+    uint16_t *rets_size
+) {
+    time_t unix_timestamp = 0;
+    enum Status status = RTC_get_time(&unix_timestamp);
+    *(time_t *)args = unix_timestamp;
+    *rets = args;
+    *rets_size = sizeof(unix_timestamp);
+    return status;
 }
--- src/helpers/rtc.h
+++ src/helpers/rtc.h
@@ -4,30 +4,17 @@
 #include <xc.h>
 #include "../commands.h"
 
-#define DS1307_I2C_DEVICE_ADDRESS        0x68
-
 #ifdef	__cplusplus
 extern "C" {
 #endif
-    
-    typedef enum {
-        DS1307_DATA_REG_SECONDS, // Clock Halt (1) | 10 Seconds (3) | Seconds (4)
-        DS1307_DATA_REG_MINUTES, // XX (1) | 10 Minutes (3) | Minutes (4)
-        DS1307_DATA_REG_HOURS,   // XX (1) | 12/24 | AM/PM | 10h | Hours (4)
-        DS1307_DATA_REG_DAYS,    // XX (5) | Day (3)
-        DS1307_DATA_REG_DATES,   // XX (2) | 10 Dates (2) | Dates (4)
-        DS1307_DATA_REG_MONTHS,  // XX (3) | 10 Months (1) | Month (4)
-        DS1307_DATA_REG_YEARS,   // 10 Years (4) | Year (4) [00 - 99]
-        DS1307_DATA_REG_CONTROL  // OUT (1) | XX (2) | SQWE (1) | XX (2) | RS1:0 (2)
-    } DS1307_DATA_REG;
 
     /**
      * @brief Updates the time in DS1307 real-time clock
-     * 
+     *
      * @description
      * This command takes eight arguments over serial
      * 1. (uint8) seconds
-     *    This 8-bit variable defines the second in time and it should have the 
+     *    This 8-bit variable defines the second in time and it should have the
      *    following format and Clock Halt bit needs to be 0 for the RTC to work.
      *    | Clock Halt (1) | 10 Seconds (3) | Seconds (4) |
      * 2. (uint8) minutes
@@ -39,11 +26,11 @@ extern "C" {
      *    following format. (Note: XX is a don't care bit)
      *    | XX (1) | 12/24 | AM/PM | 10h | Hours (4) |
      * 4. (uint8) day
-     *    This 8-bit variable defines the week of the day (0 - Monday, ...) and 
+     *    This 8-bit variable defines the week of the day (0 - Monday, ...) and
      *    it should have the following format. (Note: XX is a don't care bit)
      *    | XX (5) | Day (3) |
      * 5. (uint8) date
-     *    This 8-bit variable defines the date in month and it should have the 
+     *    This 8-bit variable defines the date in month and it should have the
      *    following format. (Note: XX is a don't care bit)
      *    | // XX (2) | 10 Dates (2) | Dates (4) |
      * 6. (uint8) month
@@ -55,21 +42,21 @@ extern "C" {
      *    following format.
      *    | 10 Years (4) | Year (4) |
      * 8. (uint8) control
-     *    This is a control sequence to enable the SQR pin in DS1307 module and 
+     *    This is a control sequence to enable the SQR pin in DS1307 module and
      *    to power up the timing clock. We are not using the SQR functions. The
      *    MSB needs to be set to high to enable to clock.
      *    | OUT (1) | XX (2) | SQWE (1) | XX (2) | RS1:0 (2) |
-     * 
+     *
      * This method does not pass any messages over the serial back to host.
      * This method decodes the unix_timestamp passed as param, converts it into RTC format and
      * stores the value in RTC hardware.
      *
      * @Param
      *      const* const unix_timestamp: Time to be stored in 32 bit unix timestamp format.
-     * 
+     *
      * @return SUCCESS, FAILED
      */
-    response_t RTC_SetTime(uint32_t const* const unix_timestamp);
+    enum Status RTC_set_time(time_t const *unix_timestamp);
 
     /**
      * @brief Stores time in RTC Hardware.
@@ -79,44 +66,31 @@ extern "C" {
      *
      * @return SUCCESS, FAILED
      */
-    response_t RTC_CmdSetTime(void);
+    enum Status RTC_cmd_set_time(
+        uint8_t const *args,
+        uint16_t args_size,
+        uint8_t **rets,
+        uint16_t *rets_size
+    );
 
     /**
-     * @brief Updates a single time parameter in DS1307 real-time clock
-     * 
-     * @description
-     * This command takes two arguments over serial.
-     * 1. (uint8) address
-     *    This 8-bit variable defines one of the 8 registers in DS1307 as defined
-     *    in DS1307_DATA_REG enum.
-     * 2. (uint8) data
-     *    This variable has the data to be written to the register defined using
-     *    the address variable.
-     * 
-     * This method does not pass any messages over the serial back to host.
-     * 
-     * @return SUCCESS, FAILED
-     */
-    response_t RTC_SetDigit(void);
-    
-    /**
      * @brief Fetch time from DS1307 real-time clock
-     * 
+     *
      * @description
      * This command reads the real time value from clock module and send it over
      * the serial back to host. There will be seven byte transfers from PSLab to
-     * host with the order following the register pattern defined in the 
+     * host with the order following the register pattern defined in the
      * DS1307_DATA_REG enum.
-     * 
+     *
      * This method will pass 8 unsigned bytes to the host over serial and host
      * will need to read all 7 bytes to complete the transaction.
      *
      * @param
      * *unix_timestamp - Time in RTC hardware is converted to unix_timestamp and stored in the parameter
-     * 
+     *
      * @return SUCCESS, FAILED
      */
-    response_t RTC_GetTime(uint32_t* unix_timestamp);
+    enum Status RTC_get_time(time_t *unix_timestamp);
 
     /**
      * @Brief Fetches time from RTC Hardware in unix_timestamp format.
@@ -126,28 +100,15 @@ extern "C" {
      *
      * @return SUCCESS, FAILED
      */
-    response_t RTC_CmdGetTime(void);
-    /**
-     * @brief Fetch a single time parameter from DS1307 real-time clock
-     * 
-     * @description
-     * This command takes one argument over serial allowing the host to read a 
-     * single register value from DS1307 without having to read data from the 
-     * whole register set.
-     * 1. (uint8) register
-     *    This variable defines the register value to read the data from. It has
-     *    to be one of the registers in DS1307_DATA_REG enum.
-     * 
-     * This method will pass one unsigned byte with the data content in the called
-     * register to the host over serial
-     * 
-     * @return SUCCESS, FAILED
-     */
-    response_t RTC_GetDigit(void);
+    enum Status RTC_cmd_get_time(
+        uint8_t *args,
+        uint16_t args_size,
+        uint8_t **rets,
+        uint16_t *rets_size
+    );
 
 #ifdef	__cplusplus
 }
 #endif
 
 #endif	/* RTC_H */
-
