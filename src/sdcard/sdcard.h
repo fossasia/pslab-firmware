0fb0bc5313f5ca15d7bc79aacac6b6fec6550f4c
Alexander Bessman <alexander.bessman@gmail.com>
2025-02-27 11:43:22 +0100

    Refactor sdcard
    The previous model of atomic read/write functions does not work with
    the new protocol. Host must now explicitly mount drive, open file,
    repeatedly write blocks of data, close file unmount drive.

--- src/sdcard/sdcard.h
+++ src/sdcard/sdcard.h
@@ -1,100 +1,134 @@
 #ifndef _SDCARD_H
 #define _SDCARD_H
 
+#include <stdint.h>
+
 #include "../commands.h"
 
-/**
- * @brief Write data to a file on the SD-card.
- *
- * @details UART transaction order:
- *             Rx[1-12]    filename
- *             Rx[1]       mode
- *     <DEBUG> Tx[1]       result_mount
- *             Tx[1]       result_open
- *             Rx[4]       data_size
- *             loop until entire file written:
- *                 Rx[512] data
- *                 Tx[1]   result_write
- *             Tx[4]       total_bytes
- *     <DEBUG> Tx[1]       result_close
- *     <DEBUG> Tx[1]       result_unmount
- *
- * @param filename TCHAR[12]
- *     Null-terminated 8.3 filename. 8.3 filenames are limited to at most
- *     eight characters (after any directory specifier), followed
- *     optionally by a filename extension consisting of a period and at
- *     most three further characters.
- * @param mode BYTE
- *     Access mode. A combination of:
- *     OPEN_EXISTING = 0x00
- *     CREATE_NEW = 0x04
- *     CREATE_ALWAYS = 0x08
- *     OPEN_ALWAYS = 0x10
- *     OPEN_APPEND = 0x30
- * @param data_size FSIZE_t
- *     Number of bytes to write to the file.
- * @param data TCHAR[512] [repeats until data_size written]
- *     Data to write to the SD-card.
- *
- * @return result_open FResult
- * @return result_write FResult [repeats once per write]
- * @return bytes_written FSIZE_t
- *     Number of bytes which were actually written to the SD-card. This
- *     should be equal to `data_size` if all went well.
- *
- * @return DO_NOT_BOTHER
- */
-response_t SDCARD_write_file(void);
+ /**
+  * @brief Mount SD-card.
+  *
+  * @return FRESULT result
+  * @return enum Status status
+  */
+enum Status SDCARD_mount(
+   uint8_t *args,
+   uint16_t args_size,
+   uint8_t **rets,
+   uint16_t *rets_size
+);
 
 /**
- * @brief Read data from a file on the SD-card.
+ * @brief Unmount SD-card.
  *
- * @details UART transaction order:
- *             Rx[1-12]    filename
- *     <DEBUG> Tx[1]       result_mount
- *     <DEBUG> Tx[1]       result_open
- *     <DEBUG> Tx[1]       result_stat
- *             Tx[4]       data_size
- *             loop until entire file read:
- *                 Tx[512] data
- *             Tx[4]       total_bytes
- *     <DEBUG> Tx[1]       result_close
- *     <DEBUG> Tx[1]       result_unmount
- * 
- * @param filename TCHAR[12]
- *     Null-terminated 8.3 filename. 8.3 filenames are limited to at most
- *     eight characters (after any directory specifier), followed
- *     optionally by a filename extension consisting of a period and at
- *     most three further characters.
- * @return data_size FSIZE_t
- *         Size of the file.
- * @return data TCHAR[512] [repeats until data_size read]
- *     Data read from the SD-card.
- * @return total_bytes FSIZE_t
- *     Number of bytes which were actually read from the SD-card. This
- *     should be equal to `file_size` if all went well. If it is not
- *     equal to `file_size`, any bytes beyond `bytes_read` are invalid.
- *
- * @return DO_NOT_BOTHER
+  * @return FRESULT result
+  * @return enum Status status
  */
-response_t SDCARD_read_file(void);
+enum Status SDCARD_unmount(
+   uint8_t *args,
+   uint16_t args_size,
+   uint8_t **rets,
+   uint16_t *rets_size
+);
 
 /**
- * @brief Get metadata of file on SD-card.
+ * @brief Get info about file on SD-card.
  *
- * @details See documentation of FatFS FILINFO.
-        UART transaction order:
- *             Rx[1-12]    filename
- *     <DEBUG> Tx[1]       result_mount
- *     <DEBUG> Tx[1]       result_stat
- *             Tx[4]       data_size
- *             Tx[2]       modification_date
- *             Tx[2]       modification_time
- *             Tx[1]       file_attributes
- *     <DEBUG> Tx[1]       result_unmount
+ * @param uint8_t fn_size
+ *   1 < n <= 13 including termination.
+ * @param char *fn
+ *   Null-terminated string.
  *
- * @return SUCCESS
+ * @return FILINFO info
+ * @return FRESULT result
+ * @return enum Status status
  */
-response_t SDCARD_get_file_info(void);
+enum Status SDCARD_stat(
+    uint8_t *args,
+    uint16_t args_size,
+    uint8_t **rets,
+    uint16_t *rets_size
+);
+
+/**
+ * @brief Open file for reading or writing.
+ *
+ * @param uint8_t fn_size
+ *   1 < n <= 13 including termination.
+ * @param uint8_t mode
+ *   READ           0x01
+ *   WRITE          0x02
+ *   OPEN_EXISTING  0x00
+ *   CREATE_NEW     0x04
+ *   CREATE_ALWAYS  0x08
+ *   OPEN_ALWAYS    0x10
+ *   OPEN_APPEND    0x30
+ * @param char *fn
+ *   Null-terminated string.
+ *
+ * @return FRESULT result
+ * @return enum Status status
+ */
+enum Status SDCARD_open(
+    uint8_t *args,
+    uint16_t args_size,
+    uint8_t **rets,
+    uint16_t *rets_size
+);
+
+/**
+ * @brief Close file.
+ *
+ * @return FRESULT result
+ * @return enum Status status
+ */
+enum Status SDCARD_close(
+    uint8_t *args,
+    uint16_t args_size,
+    uint8_t **rets,
+    uint16_t *rets_size
+);
+
+/**
+ * @brief Write a block of data to file.
+ *
+ * @details
+ *   This function can be called repeatedly to write sequential data.
+ *
+ * @param uint16_t data_size
+ *   0 < n <= 512
+ * @param uint8_t *data
+ *
+ * @return FRESULT result
+ * @return uint16_t bytes_written
+ * @return enum Status status
+ */
+enum Status SDCARD_write(
+    uint8_t *args,
+    uint16_t args_size,
+    uint8_t **rets,
+    uint16_t *rets_size
+);
+
+/**
+ * @brief Read data from file.
+ *
+ * @details
+ *   This function can be called repeatedly to read sequential data.
+ *
+ * @param uint16_t data_size
+ *   0 < n <= 512
+ *
+ * @return FRESULT result
+ * @return uint16_t bytes_read
+ * @return uint8_t *data
+ * @return enum Status status
+ */
+enum Status SDCARD_read(
+    uint8_t *args,
+    uint16_t args_size,
+    uint8_t **rets,
+    uint16_t *rets_size
+);
 
 #endif // _SDCARD_H
