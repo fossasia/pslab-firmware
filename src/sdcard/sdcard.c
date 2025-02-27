eaf9bfd63cf9e270660cf68966a817b334d75b41
Alexander Bessman <alexander.bessman@gmail.com>
2025-02-27 11:43:22 +0100

    Refactor sdcard
    The previous model of atomic read/write functions does not work with
    the new protocol. Host must now explicitly mount drive, open file,
    repeatedly write blocks of data, close file unmount drive.

--- src/sdcard/sdcard.c
+++ src/sdcard/sdcard.c
@@ -1,109 +1,233 @@
-#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
 
-#include "../bus/uart/uart.h"
 #include "../commands.h"
 #include "fatfs/ff.h"
 #include "fatfs/ffconf.h"
-#include "../registers/system/watchdog.h"
 
-#define SFN_MAX 8
+#define SFN_MIN 2 // At least one character + null terminator.
+#define SFN_NAME 8
 #define SFN_SUFFIX_LEN 4 // Period + at most three chars.
+#define SFN_MAX (SFN_NAME + SFN_SUFFIX_LEN + 1) // + null terminator.
 #define BUF_MAX FF_MIN_SS
 
-static void get_filename(TCHAR* const fn_buf, UINT const size) {
-    for (UINT i = 0; i < size; ++i) {
-        if (!(fn_buf[i] = UART1_Read())) {
-            // Received null-terminator.
-            return;
-        }
-    }
-    // FatFS handles non-terminated strings gracefully, so no need to do a
-    // manual check for null-termination.
+FATFS *g_drive_p = NULL;
+FIL *g_file_p = NULL;
+
+enum Status SDCARD_mount(
+    uint8_t *const args,
+    __attribute__((unused)) uint16_t const args_size,
+    uint8_t **rets,
+    uint16_t *rets_size
+) {
+    union Output {
+        FRESULT res;
+        uint8_t *buffer;
+    } output = {0};
+    output.res = f_mount(g_drive_p, "0:", 1);
+    *rets = args;
+    *rets_size = sizeof(output);
+    memcpy(*rets, output.buffer, *rets_size);
+    return output.res ? E_FAILED : E_OK;
 }
 
-response_t SDCARD_write_file(void) {
-    // +1 is null-terminator.
-    size_t const filename_size = SFN_MAX + SFN_SUFFIX_LEN + 1;
-    TCHAR filename[filename_size];
-    get_filename(filename, filename_size);
-    BYTE const mode = UART1_Read();
-
-    FATFS drive;
-
-    FIL file;
-    // Host must wait for f_open before sending data.
-    UART1_Write(f_open(&file, filename, FA_WRITE | mode));
-
-    FSIZE_t data_size = UART1_read_u32();
-    FSIZE_t bytes_written = 0;
-
-    for (
-        FSIZE_t block_size, remaining = data_size;
-        block_size = remaining > BUF_MAX ? BUF_MAX : remaining,
-        remaining;
-        remaining -= block_size
-    ) {
-        TCHAR buffer[block_size];
-
-        for (UINT i = 0; i < block_size; ++i) {
-            buffer[i] = UART1_Read();
-        }
-
-        WATCHDOG_TimerClear();
-        UINT written = 0;
-        // Host must wait for f_write before sending more data.
-        UART1_Write(f_write(&file, buffer, (UINT)block_size, &written));
-        bytes_written += written;
-    }
-
-    UART1_write_u32(bytes_written);
-    return DO_NOT_BOTHER;
+enum Status SDCARD_unmount(
+    uint8_t *const args,
+    __attribute__((unused)) uint16_t const args_size,
+    uint8_t **rets,
+    uint16_t *rets_size
+) {
+    union Output {
+        FRESULT res;
+        uint8_t *buffer;
+    } output = {0};
+    g_drive_p = NULL;
+    output.res = f_mount(g_drive_p, "0:", 0);
+    *rets = args;
+    *rets_size = sizeof(output);
+    memcpy(*rets, output.buffer, *rets_size);
+    return output.res ? E_FAILED : E_OK;
 }
 
-response_t SDCARD_read_file(void) {
-    TCHAR filename[SFN_MAX + SFN_SUFFIX_LEN + 1]; // +1 is null-terminator.
-    get_filename(filename, sizeof filename);
+enum Status SDCARD_stat(
+    uint8_t *const args,
+    uint16_t const args_size,
+    uint8_t **rets,
+    uint16_t *rets_size
+) {
+    union Input {
+        struct {
+            uint8_t fn_size;
+            char *fn;
+        };
+        uint8_t const *buffer;
+    } input = {{0}};
 
-    FATFS drive;
-    FIL file;
-    FILINFO info = {0, 0, 0, 0, {0}};
-    UART1_write_u32(info.fsize);
-    FSIZE_t bytes_read = 0;
-
-    for (
-        FSIZE_t block_size, remaining = info.fsize;
-        block_size = remaining > BUF_MAX ? BUF_MAX : remaining,
-        remaining;
-        remaining -= block_size
-    ) {
-        WATCHDOG_TimerClear();
-        UINT read = 0;
-        TCHAR buffer[block_size];
-        f_read(&file, &buffer, (UINT)block_size, &read);
-        bytes_read += read;
-
-        for (UINT i = 0; i < block_size; ++i) {
-            UART1_Write(buffer[i]);
-        }
+    if (args_size < sizeof(input.fn_size)) {
+        return E_BAD_ARGSIZE;
     }
 
-    UART1_write_u32(bytes_read);
-    return DO_NOT_BOTHER;
+    input.buffer = args;
+
+    if (input.fn_size < SFN_MIN) {
+        return E_BAD_SIZE;
+    }
+
+    if (input.fn_size > SFN_MAX) {
+        return E_BAD_SIZE;
+    }
+
+    union Output {
+        struct {
+            FILINFO info;
+            FRESULT res;
+        };
+        uint8_t *buffer;
+    } output = {.info = {0}, .res = 0};
+    output.res = f_stat(input.fn, &output.info);
+    *rets = args;
+    *rets_size = sizeof(output);
+    memcpy(*rets, output.buffer, *rets_size);
+    return output.res ? E_FAILED : E_OK;
 }
 
-response_t SDCARD_get_file_info(void) {
-    TCHAR filename[SFN_MAX + SFN_SUFFIX_LEN + 1]; // +1 is null-terminator.
-    get_filename(filename, sizeof filename);
+enum Status SDCARD_open(
+    uint8_t *const args,
+    uint16_t const args_size,
+    uint8_t **rets,
+    uint16_t *rets_size
+) {
+    union Input {
+        struct {
+            uint8_t fn_size;
+            uint8_t mode;
+            char *fn;
+        };
+        uint8_t const *buffer;
+    } input = {{0}};
 
-    FATFS drive;
-    FILINFO info = {0, 0, 0, 0, {0}};
+    if (args_size < sizeof(input.fn_size)) {
+        return E_BAD_ARGSIZE;
+    }
 
-    UART1_write_u32(info.fsize);
-    UART1_WriteInt(info.fdate);
-    UART1_WriteInt(info.ftime);
-    UART1_Write(info.fattrib);
+    input.buffer = args;
 
-    return SUCCESS;
+    if (input.fn_size < SFN_MIN) {
+        return E_BAD_SIZE;
+    }
+
+    if (input.fn_size > SFN_MAX) {
+        return E_BAD_SIZE;
+    }
+
+    union Output {
+        struct {
+            FRESULT res;
+        };
+        uint8_t *buffer;
+    } output = {{0}};
+    output.res = f_open(g_file_p, input.fn, input.mode);
+    *rets = args;
+    *rets_size = sizeof(output);
+    memcpy(*rets, output.buffer, *rets_size);
+    return output.res ? E_FAILED : E_OK;
+}
+
+enum Status SDCARD_close(
+    uint8_t *const args,
+    __attribute__((unused)) uint16_t const args_size,
+    uint8_t **rets,
+    uint16_t *rets_size
+) {
+    union Output {
+        struct {
+            FRESULT res;
+        };
+        uint8_t *buffer;
+    } output = {{0}};
+    output.res = f_close(g_file_p);
+    g_file_p = NULL;
+    *rets = args;
+    *rets_size = sizeof(output);
+    memcpy(*rets, output.buffer, *rets_size);
+    return output.res ? E_FAILED : E_OK;
+}
+
+enum Status SDCARD_write(
+    uint8_t *const args,
+    uint16_t const args_size,
+    uint8_t **rets,
+    uint16_t *rets_size
+) {
+    union Input {
+        struct {
+            uint16_t data_size;
+            uint8_t *data;
+        };
+        uint8_t const *buffer;
+    } input = {{0}};
+
+    if (args_size < sizeof(input.data_size)) {
+        return E_BAD_ARGSIZE;
+    }
+
+    input.buffer = args;
+
+    if (args_size != (sizeof(input.data_size) + input.data_size)) {
+        return E_BAD_ARGSIZE;
+    }
+
+    if (input.data_size > BUF_MAX) {
+        return E_BAD_SIZE;
+    }
+
+    union Output {
+        struct {
+            FRESULT res;
+            uint16_t written;
+        };
+        uint8_t *buffer;
+    } output = {{0}};
+    output.res = f_write(g_file_p, input.data, input.data_size, &output.written);
+    *rets = args;
+    *rets_size = sizeof(output);
+    memcpy(*rets, output.buffer, *rets_size);
+    return output.res ? E_FAILED : E_OK;
+}
+
+enum Status SDCARD_read(
+    uint8_t *const args,
+    uint16_t const args_size,
+    uint8_t **rets,
+    uint16_t *rets_size
+) {
+    union Input {
+        struct {
+            uint16_t data_size;
+        };
+        uint8_t const *buffer;
+    } input = {{0}};
+
+    if (args_size != sizeof(input)) {
+        return E_BAD_ARGSIZE;
+    }
+
+    input.buffer = args;
+
+    union Output {
+        struct {
+            FRESULT res;
+            uint16_t read;
+            uint8_t *data;
+        };
+        uint8_t *buffer;
+    } output = {{0}};
+    *rets = args;
+    output.buffer = *rets;
+    output.res = f_read(g_file_p, output.data, input.data_size, &output.read);
+    *rets = args;
+    *rets_size = sizeof(output);
+    memcpy(*rets, output.buffer, *rets_size);
+    return output.res ? E_FAILED : E_OK;
 }
